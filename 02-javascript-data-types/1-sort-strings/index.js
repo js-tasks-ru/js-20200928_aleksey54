73d89db1ed785933117b6c82fa2df0871d9236e6
Alexey <alekseiovechkin@gmail.com>
2020-10-02 20:43:17 +0300

    sort string task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let orderCode = 0;
+    param === 'asc' ? orderCode = 1 : orderCode = -1;
+    return [...arr].sort(
+      function(a,b) {
+        return a.localeCompare(b, 'ru', {caseFirst: 'upper'}) * orderCode;
+      }
+      )
 }
